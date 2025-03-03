24774e8bcd19580097460a8d622052bb56eef7e9
joao.ribeiro <ata4l4k5t3npr5p67ytqva5xlodr2egg6ijclyhz6chb4djxxksq>
2025-03-03 11:08:14 -0300

    Commit Aula 4

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Meu irmão, meu grande amigo! Você é alguém que nunca mede esforços nem tempo para que tudo na minha vida aconteça da melhor forma possível, seja no lado profissional ou pessoal. Sua lealdade e generosidade são raras, e eu sou imensamente grato por ter você ao meu lado. Te amo, meu irmão! Que você continue sendo essa pessoa iluminada, cheia de compaixão e que Deus trouxe conquistas sem medidas sobre a sua vida. Você merece tudo de melhor! </h1>
+}
+
+export default Home;
